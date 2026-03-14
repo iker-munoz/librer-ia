3ce321fa9172252c11014900e114b0b895fa00e1
iker-munoz <iker.munoz35@educa.madrid.org>
2026-03-14 17:26:34 +0100

    Added basic page layout

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<div class="navbar">
+    <h1>LibrerIA</h1>
+    <hr>
+    <div class="navigation">
+        
+    </div>
+</div>
+
+<style>
+    .navbar {
+        width: 400px;
+        height: 100%;
+        padding: 20px;
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    .navigation {
+        width: 100%;
+        height: fit-content;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import Navbar from '../components/Navbar.svelte';
+
 	let { children } = $props();
 </script>
 
@@ -6,4 +8,56 @@
     <title>LibrerIA</title>
 </svelte:head>
 
-{@render children()}
+<Navbar/>
+<div class="content-wrapper">
+    <div class="content">
+        {@render children()}
+    </div>
+</div>
+
+<style>
+    :global(html, body) {
+        width: 100vw;
+        height: 100vh;
+        padding: 0px;
+        margin: 0px;
+        background-color: #101010;
+        color: #F0F0F0;
+    }
+
+    :global(body) {
+        display: flex;
+    }
+
+    :global(h1) {
+        margin: 0px;
+        font-size: 24px;
+        font-weight: 600;
+    }
+
+    :global(p) {
+        margin: 0px;
+        font-size: 16px;
+    }
+
+    :global(hr) {
+        width: 100%;
+        height: 1px;
+        background-color: #202020;
+        border: 0px;
+        margin: 0px;
+    }
+
+    .content-wrapper {
+        width: calc(100% - 20px);
+        height: calc(100% - 20px);
+        margin: 10px;
+        padding: 50px;
+        border: 1px solid #202020;
+        border-radius: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+    }
+</style>
